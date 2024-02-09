d282825ed406c6f702cd36d23122735168a94758
Rizwan Chaudry <chaudryrizwan@gmail.com>
2024-02-09 13:18:00 -0500

    Exercise 1.2 done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,7 +6,7 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
       <p>
@@ -18,6 +18,16 @@ const Content = (props) => {
       <p>
         {props.part3} {props.exercises3}
       </p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part1 = {props.part1} exercises1 = {props.exercises1} />
+      <Part part2 = {props.part2} exercises2 = {props.exercises2} />
+      <Part part3 = {props.part3} exercises3 = {props.exercises3} />
     </>
   )
 }
